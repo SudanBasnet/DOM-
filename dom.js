8a08f047999d9b271bcf4123eea58c60a9800794
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-09 23:34:56 +1000

    first commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+// const elm = document.getElementsByid"info");
+// const elm = document.getElementsByClassName("info");
+// const elm = document.getElementsByTagName("div");
+// const elm = document.querySelector(".first");
+// const elm = document.querySelector(".info");
+// const elm = document.querySelector("#first");
+const elm = document.querySelectorAll(".info >div p");
+console.log(elm);
