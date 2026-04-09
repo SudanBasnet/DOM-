e92ac4463348e328c8efe08da52f361fb3773c7d
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-10 00:06:35 +1000

    selecting element nodes

--- dom.js
+++ dom.js
@@ -4,5 +4,30 @@
 // const elm = document.querySelector(".first");
 // const elm = document.querySelector(".info");
 // const elm = document.querySelector("#first");
-const elm = document.querySelectorAll(".info >div p");
-console.log(elm);
+// const elm = document.querySelectorAll(".info >div p");
+// const value = elm.innerText;
+// const value = elm.innerHTML;
+// console.log(value);
+
+// document.write("added more content"); //adds to end of the document //below script
+
+// const elm = document.querySelector(".message");
+// const val = elm.innerText;
+// elm.innerText = "just got text updated";
+// elm.innerHTML = "<h1>just got text updated</h1>";
+// console.log(val);
+
+// elm.appendChild(textNode);
+// const elm = document.querySelector("body");
+// const textNode = document.createTextNode("hi this is dynamic content");
+// console.log(elm);
+
+// const paraElm = document.createElement("p");
+// paraElm.innerText = "Hi this is dynamic content";
+
+// elm.appendChild(paraElm);
+// const e1 = document.querySelector(".display");
+// elm.insertBefore(paraElm, document.querySelector(".display"));
+
+// elm.removeChild(document.querySelector(`[href="google.com"]`));
+// elm.replaceChild(paraElm, document.querySelector(`[href="google.com"]`));
